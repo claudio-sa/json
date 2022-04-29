a3a955b99842cde744efc43e472b9671394d57e9
Eduardo Pereira <dudupereira6@gmail.com>
2022-04-29 14:57:30 -0300

    * Bug fix * Implementação parte visual * Implementação de filtros

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { useState, useFormState, useEffect, useLayoutEffect } from 'react'
-import axios from 'axios'
-import './App.css'
-import moment from 'moment' // para formatar datas etc
+import React, { useRef, useState, useEffect, useLayoutEffect } from "react"
+import axios from "axios"
+import moment from "moment" // para formatar datas etc
+import "./App.css"
+
+moment.locale("pt-br")
 
 export default function App(props) {
   var [listaGlobal, setListaGlobal] = useState([])
-  var [busca, setBusca] = useState('')
-  var [data_01, setData_01] = useState('') // da pagina do HTML
-  var [data_02, setData_02] = useState('') // da pagina do HTML
+  var [busca, setBusca] = useState("")
+  var [filterReceived, setFilterReceived] = useState(false)
+  var [filterSent, setFilterSent] = useState(false)
+  var [dataIni, setDataIni] = useState(null)
+  var [dataFim, setDataFim] = useState(null)
+  const refdataIni = useRef()
+  const refdataFim = useRef()
 
   const lowerBusca = busca.toLowerCase()
 
-  // FUNCAO ... faz a leitura e o tratamento
   async function get_an_URL(umaURL) {
     const response_data = await axios
       .create()
       .get(umaURL)
       .then((response) => response.data)
       .catch((err) => {
-        console.error('ops! ocorreu um erro' + err)
+        console.error("ops! ocorreu um erro" + err)
       })
 
     const iguais =
-      '\n============================================================================================================================='
-
-    // Cria um array separados e são separado onde há =====
+      "\n============================================================================================================================="
     const array_sem_iguais = response_data.split(iguais)
 
-    // Deste array toma-se a parte que interessa
+    // console.log("array_sem_iguais", array_sem_iguais)
+
     const array_TEMP = array_sem_iguais.map((obj) => {
-      return obj.split('==>')[1]
+      return obj.split("==>")[1]
     })
 
-    // Deste array de STRINGs ... será transformado em
-    // um ARRAY com objetos em JASON
+    // console.log("array_TEMP", array_TEMP)
+
     var array_via_map = []
     for (var i = 0; i < array_TEMP.length; i += 1) {
       try {
@@ -45,113 +47,163 @@ export default function App(props) {
           array_via_map.push(parsed)
         }
       } catch (e) {
-        console.log('Obj NAO OK:', '==>', parsed)
+        // console.log("Obj NAO OK:", "==>", parsed)
       }
     }
-    //console.log('Return do get_A_URL')
     const size_ARRAY = array_via_map.length - 1
-    console.log('Primeira Amostra:', array_via_map[1])
-    console.log('Ultima amostra:', size_ARRAY, '::', array_via_map[size_ARRAY])
-    console.log(
-      '....',
-      moment(array_via_map[1].message.receivedAt).format('DD/MM/YYYY'),
-    )
+
+    // console.log("array_via_map", array_via_map)
+
     return array_via_map
   }
 
-  // os USEEFFECT
+  function compare(a, b) {
+    if (
+      a.message.receivedAt > b.message.receivedAt ||
+      a.message.sentAt > b.message.sentAt
+    )
+      return -1
+    if (
+      a.message.receivedAt < b.message.receivedAt ||
+      a.message.sentAt < b.message.sentAt
+    )
+      return 1
+    return 0
+  }
+
+  function converteData(DataDDMMYY) {
+    const dataSplit = DataDDMMYY.split("-")
+    const novaData = new Date(
+      parseInt(dataSplit[2], 10),
+      parseInt(dataSplit[1], 10) - 1,
+      parseInt(dataSplit[0], 10)
+    )
+    return novaData
+  }
 
   listaGlobal = listaGlobal
+    .sort(compare)
     .filter((contactItem) => contactItem.message.contact.name !== null)
-    .filter((contactItem) => contactItem.message.contact.name !== '')
+    .filter((contactItem) => contactItem.message.contact.name !== "")
     .filter((contactItem) => contactItem.message.contact.name !== undefined)
-    // .filter(
-    //   (contactItem) =>
-    //     moment(contactItem.message.receivedAt).format('DD/MM/YYYY') >=
-    //     moment(data_01).format('DD/MM/YYYY'),
-    // )
-    ////
-    .filter(
-      (contactItem) =>
-        new Date(moment(contactItem.message.receivedAt).format('DD/MM/YYYY')) >=
-        new Date(moment(data_01).format('DD/MM/YYYY')),
-    )
-    // .filter((contactItem) => contactItem.deliveredAt >= data_02)
-    // moment(array_via_map[1].message.receivedAt).format('DD/MM/YYYY'),
-    .filter(
-      (contactItem) =>
-        contactItem.message.contact.name.toLowerCase().includes(lowerBusca),
-      //contactItem.message.contact.name.toLowerCase() === lowerBusca,
-
-      //  .filter((contactItem) =>
-      //       (contactItem) =>contactItem.message.receivedAt.includes(data_01)
+    .filter((contactItem) =>
+      contactItem.message.contact.name.toLowerCase().includes(lowerBusca)
     )
 
-  // testes.message.contact.name === 'Eduardo Bento',
+  if (dataIni) {
+    listaGlobal = listaGlobal.filter(
+      (contactItem) =>
+        moment(contactItem.message.receivedAt).format("YYYY-MM-DD") >=
+          dataIni ||
+        moment(contactItem.message.sentAt).format("YYYY-MM-DD") >= dataIni
+    )
+  }
+  if (dataFim) {
+    listaGlobal = listaGlobal.filter(
+      (contactItem) =>
+        moment(contactItem.message.receivedAt).format("YYYY-MM-DD") <=
+          dataFim ||
+        moment(contactItem.message.sentAt).format("YYYY-MM-DD") <= dataFim
+    )
+  }
+  if (filterReceived) {
+    listaGlobal = listaGlobal.filter(
+      (contactItem) => contactItem.message.direction === "incoming"
+    )
+  }
+  if (filterSent) {
+    listaGlobal = listaGlobal.filter(
+      (contactItem) => contactItem.message.direction === "outgoing"
+    )
+  }
 
   useEffect(() => {
-    var data = moment(data_01).format('DD-MM-YYYY')
+    console.log(new Date(dataIni).getDate())
+    console.log(new Date("2022-04-29T00:41:23").getDate())
+    console.log(converteData("2022-04-29T16:52:49"))
+    console.log(dataIni)
+    console.log(dataFim)
+  }, [dataIni, dataFim])
 
-    console.log('useEffect ', data)
-    // console.log(
-    //   'useEffect ',
-    //   new Date(data_01.getDay(), data_01.getMonth(), data_01.getYear()),
-    // )
-  }, [data_01])
-
-  useEffect(async () => {
-    const res = await get_an_URL('https://whatstv-api.herokuapp.com/logrobbu')
-    console.log('useEffect')
+  useLayoutEffect(async () => {
+    const res = await get_an_URL("https://whatstv-api.herokuapp.com/logrobbu")
     setListaGlobal(res)
   }, [])
 
-  // RETORNO DA PAGINA DO APP
   return (
     <div className="main">
       <header className="logHeader">
-        <h1> Relatório Invemio - TV Aparecida </h1>
-
+        <h2> Log de Mesagens: {listaGlobal.length} </h2>
         <div className="logFilter">
-          <label for="busca"> Busca: </label>
+          <div className="">
+            <div className="filterReceived">
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  setFilterReceived(!filterReceived)
+                }}
+              />
+              <label>Somente Recebidas</label>
+            </div>
+            <div className="filterReceived">
+              <input
+                type="checkbox"
+                onChange={(e) => {
+                  setFilterSent(!filterSent)
+                }}
+              />
+              <label>Somente Enviadas</label>
+            </div>
+          </div>
           <input
             type="search"
-            placeholder="Busca"
+            placeholder="Nome"
             value={busca}
             onChange={(e) => setBusca(e.target.value)}
           />
-        </div>
-
-        <div className="dataFilter">
-          <label for="busca"> Data 01: </label>
           <input
-            type="date" // "datetime-local"  funciona tambem
+            type="text"
+            ref={refdataIni}
             placeholder="Data Inicial"
-            // value={data_01}
-            onChange={(e) => setData_01(e.target.value)}
+            onFocus={() => (refdataIni.current.type = "date")}
+            onBlur={() =>
+              refdataIni.current.value
+                ? (refdataIni.current.type = "date")
+                : (refdataIni.current.type = "text")
+            }
+            onChange={(e) => setDataIni(e.target.value)}
+          />
+          <input
+            type="text"
+            ref={refdataFim}
+            onFocus={() => (refdataFim.current.type = "date")}
+            onBlur={() =>
+              refdataFim.current.value
+                ? (refdataFim.current.type = "date")
+                : (refdataFim.current.type = "text")
+            }
+            placeholder="Data Final"
+            onChange={(e) => setDataFim(e.target.value)}
           />
         </div>
       </header>
       <div className="content">
         {listaGlobal.map((obj) => (
           <div className="item">
-            <p>Origem: {obj.message.contact.name}</p>
+            <p>{obj.message.contact.name}</p>
             <p>
-              Telefone: {obj.message.contact.mainWhatsapp.countryCode}
+              {obj.message.contact.mainWhatsapp.countryCode}
               {obj.message.contact.mainWhatsapp.phoneNumber}
             </p>
-            <p>Tipo: {obj.message.direction} </p>
+            <p> {obj.message.direction} </p>
             <p>
-              Data: {moment(obj.message.receivedAt).format('DD/MM/YYYY')} Hora:{' '}
-              {moment(obj.message.receivedAt).format('hh:mm:ss')}
+              {moment(obj.message.receivedAt).format("DD/MM/YYYY")}{" "}
+              {moment(obj.message.receivedAt).format("hh:mm:ss a")}
             </p>
-            <p>Texto: {obj.message.text}</p>
+            <p className="itemMessage"> {obj.message.text}</p>
           </div>
         ))}
-
-        {<p>Total: {listaGlobal.length}</p>}
       </div>
     </div>
   )
 }
-
-//export default App
